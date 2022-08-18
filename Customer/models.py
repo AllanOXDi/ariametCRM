0c34296d15e363318e06ba8c5930471ebbdd07c7
AllanOXDi <otodi.allan1@gmail.com>
2022-08-18 13:24:47 +0300

    .

--- Customer/models.py
+++ Customer/models.py
@@ -1,7 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+User = get_user_model()
+
+
+class User(AbstractUser):
+    pass
+
+
 class Customer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.IntegerField(True)
-    email = models.EmailField(max_length=100)
+    """ first_name = models.CharField(max_length=30)\n"
+        last_name = models.CharField(max_length=30)\n"
+        address = models.CharField(max_length=100)\n"
+        phone_number = models.BooleanField(default=False)")
+        email = models.EmailField(max_length=100)"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Offers(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Transaction(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
